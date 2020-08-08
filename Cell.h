e86deebc2c65852f7edc21b39a8b906340da84c5
rasoolZero <rasool.kamkar20@gmail.com>
2020-08-08 14:43:38 +0430

    Cell class added, inherited from Drawable and Transformable

---
+++ Cell.h
@@ -0,0 +1,16 @@
+#ifndef CELL_H
+#define CELL_H
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class Cell : public Drawable,public Transformable
+{
+    public:
+        Cell();
+
+    protected:
+
+    private:
+};
+
+#endif // CELL_H
