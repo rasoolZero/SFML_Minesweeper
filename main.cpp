56b668e40a71de8f75f04592aec78dd1e0e4ec4f
saeed <saeedss2244@gmail.com>
2020-08-08 11:39:12 +0430

    added basic main file and .gitignore

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+
+int main() {
+    
+    sf::RenderWindow window(VideoMode::getDesktopMode(), "SFML_Minesweeper", Style::Fullscreen);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) { //close the window on ESC keypress
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+        
+    }
+	return 0;
+}
