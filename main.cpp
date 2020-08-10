f6b65b3fc0734eee7f7b6bcd9fd32e688b5458e3
rasoolZero <rasool.kamkar20@gmail.com>
2020-08-11 01:45:13 +0430

    window focus check added to main loop

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@
 
 using namespace sf;
 
+bool isFocused;
+
 int main() {
 
-    sf::RenderWindow window(VideoMode::getDesktopMode(), "SFML_Minesweeper", Style::Fullscreen);
 
+
+    sf::RenderWindow window(VideoMode::getFullscreenModes()[0], "SFML_Minesweeper", Style::Fullscreen);
     while (window.isOpen())
     {
         Event event;
@@ -17,12 +20,19 @@ int main() {
 
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == Event::GainedFocus)
+                isFocused=true;
+            if (event.type == Event::LostFocus)
+                isFocused=false;
             if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) { //close the window on ESC keypress
                 window.close();
             }
         }
 
-        window.clear();
+        window.clear(Color::White);
+        if(isFocused){
+            //update
+        }
         window.display();
 
     }
