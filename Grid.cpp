0575a038aae00578c388b9f518b82a3156535fc0
rasoolZero <rasool.kamkar20@gmail.com>
2020-08-10 18:30:56 +0430

    basic data members and methods added to Grid

--- Grid.cpp
+++ Grid.cpp
@@ -1,6 +1,24 @@
 #include "Grid.h"
+using namespace std;
+using namespace sf;
 
-Grid::Grid()
+Grid::Grid(RenderWindow & w_ref,int width,int height) : window_ref(w_ref)
 {
-    //ctor
+    this->width=width;
+    this->height=height;
+    cells = vector<Cell>();
+    for(int i=0;i<width;i++)
+        for(int j=0;j<height;j++)
+            cells.push_back(Cell(i,j,50));
+
+}
+
+void Grid::update(){
+    draw();
+}
+
+void Grid::draw(){
+    for(auto cell : cells){
+        cell.draw(window_ref,RenderStates());
+    }
 }
--- Grid.h
+++ Grid.h
@@ -1,15 +1,25 @@
 #ifndef GRID_H
 #define GRID_H
-
+#include "Cell.h"
+#include <vector>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
 
 class Grid
 {
     public:
-        Grid();
+        Grid(RenderWindow & w_ref,int width,int height);
+        void update();
 
     protected:
 
     private:
+        vector<Cell> cells;
+        RenderWindow & window_ref;
+        int width;
+        int height;
+        void draw();
 };
 
 #endif // GRID_H
