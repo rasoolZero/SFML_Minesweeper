aba69fbb4003c824dcc1bb01e85cfeae14cccd41
rasoolZero <rasool.kamkar20@gmail.com>
2020-08-11 19:54:43 +0430

    Merge

--- main.cpp
+++ main.cpp
@@ -6,13 +6,11 @@
 using namespace sf;
 
 bool isFocused;
-
 int main() {
     isFocused = true;
 
     RenderWindow window(VideoMode::getDesktopMode(), "SFML_Minesweeper", Style::Fullscreen);
     MenuManager menu(window);
-
     while (window.isOpen())
     {
         Event event;
@@ -21,17 +19,18 @@ int main() {
             if (event.type == Event::Closed)
                 window.close();
             if (event.type == Event::GainedFocus)
-                isFocused=true;
+                isFocused = true;
             if (event.type == Event::LostFocus)
-                isFocused=false;
+                isFocused = false;
             if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) { //close the window on ESC keypress
                 window.close();
             }
         }
 
+        window.clear();
         window.clear(Color(235, 235, 250));
         if (isFocused) {
-            menu.update();
+        menu.update();
         }
         window.display();
         sleep(milliseconds(5));
