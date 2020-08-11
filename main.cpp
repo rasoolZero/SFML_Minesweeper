8f6a99953c56f7385ccc1835c4d2fd534586fd59
saeed <saeedss2244@gmail.com>
2020-08-11 11:11:50 +0430

    fixed focus issue

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 
 using namespace sf;
 
+bool isFocused;
 int main() {
+    isFocused = true;
 
     sf::RenderWindow window(VideoMode::getDesktopMode(), "SFML_Minesweeper", Style::Fullscreen);
 
@@ -17,12 +19,19 @@ int main() {
 
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == Event::GainedFocus)
+                isFocused = true;
+            if (event.type == Event::LostFocus)
+                isFocused = false;
             if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) { //close the window on ESC keypress
                 window.close();
             }
         }
 
         window.clear();
+        if (isFocused) {
+            //update
+        }
         window.display();
 
     }
